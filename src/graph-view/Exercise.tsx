a09ce67a763c77e63c670bc90ad2e982990b6f5c
Tim Austin <tim@neenjaw.com>
2020-09-29 14:47:36 -0600

    adjust <Exercise /> to use useExerciseLineStore to store the paths to be drawn

--- src/graph-view/Exercise.tsx
+++ src/graph-view/Exercise.tsx
@@ -1,47 +1,36 @@
 import React, { useEffect } from 'react'
+import { addExerciseLines, removeExerciseLines } from './useExerciseLineStore'
 
-import { ExerciseData } from './exercise-types'
+import { ExerciseState, ExerciseData } from './exercise-types'
 
 import './Exercise.css'
 
-export interface IExercise {
-  data: ExerciseData
-  connectionHandler: any
-}
-
-// TODO - on load, should register with the canvas to have its lines drawn
 // TODO - on hover, should change state from active to inactive
 // TODO - for different states (locked, unlocked, complete, etc) should change the appearance
 
-export const Exercise = ({ data, connectionHandler }: IExercise) => {
+export const Exercise = ({ data }: { data: ExerciseData }) => {
   const exerciseName = deslugify(data.slug)
-  const unlocked = true
-  const status = 'Available'
+  const active = false
+  const status = data.status ?? ExerciseState.Locked
 
   useEffect(() => {
-    const currentConnectionHandler = connectionHandler.current
-    if (!currentConnectionHandler) {
-      return
-    }
-
-    currentConnectionHandler.addExerciseLines(data.slug, [
-      ...data.prerequisites,
-    ])
-
+    addExerciseLines(data.slug, [...data.prerequisites], active)
     return () => {
-      if (!currentConnectionHandler) {
-        return
-      }
+      removeExerciseLines(data.slug)
     }
-  }, [connectionHandler, data.slug, data.prerequisites])
+  }, [data.prerequisites, data.slug, active])
 
   return (
-    <div className="exercise__card" data-exercise-slug="">
+    <div
+      className={`exercise__card ${active ? 'active_exercise' : ''}`}
+      data-exercise-slug={data.slug}
+      data-exercise-status={status}
+    >
       <div className="exercise__display">
         <div className="exercise__display-name">{exerciseName}</div>
         <div className="exercise__display-stats"></div>
       </div>
-      {unlocked && <div className="exercise__status">{status}</div>}
+      {/* render another component here for the status */}
     </div>
   )
 }
