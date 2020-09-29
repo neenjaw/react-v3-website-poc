4f4331a62e944f77d64d7b99b269cc651a53a66f
Tim Austin <tim@neenjaw.com>
2020-09-29 16:03:56 -0600

    add status to data fixture

--- src/graph-view/data.ts
+++ src/graph-view/data.ts
@@ -1,4 +1,4 @@
-import { ExerciseLayersData } from './exercise-types'
+import { ExerciseLayersData, ExerciseState } from './exercise-types'
 
 export const data: ExerciseLayersData = [
   [
@@ -8,6 +8,7 @@ export const data: ExerciseLayersData = [
       uuid: 'c29c6092-9d44-4f21-8138-b873384fd90b',
       concepts: ['basics'],
       prerequisites: [],
+      status: ExerciseState.Locked,
     },
   ],
   [
@@ -17,6 +18,7 @@ export const data: ExerciseLayersData = [
       uuid: '5e743355-1ef3-4b5d-b59d-03bbc9697e6c',
       concepts: ['booleans'],
       prerequisites: ['basics'],
+      status: ExerciseState.Locked,
     },
     {
       index: 2,
@@ -24,6 +26,7 @@ export const data: ExerciseLayersData = [
       uuid: 'fee79e03-1496-476f-964f-e60632cb13dc',
       concepts: ['integers', 'floating-point-numbers'],
       prerequisites: ['basics'],
+      status: ExerciseState.Locked,
     },
     {
       index: 6,
@@ -31,6 +34,7 @@ export const data: ExerciseLayersData = [
       uuid: '1e3ceb20-715c-4157-9192-13284217affc',
       concepts: ['anonymous-functions', 'closures', 'bit-manipulation'],
       prerequisites: ['basics'],
+      status: ExerciseState.Locked,
     },
   ],
   [
@@ -40,6 +44,7 @@ export const data: ExerciseLayersData = [
       uuid: 'b26a2f11-d80a-42b9-881a-3575a83ad211',
       concepts: ['conditionals', 'atoms'],
       prerequisites: ['booleans'],
+      status: ExerciseState.Locked,
     },
     {
       index: 4,
@@ -47,6 +52,7 @@ export const data: ExerciseLayersData = [
       uuid: '3c53b340-680e-45c1-8979-3d79b441a65e',
       concepts: ['lists', 'string-literals'],
       prerequisites: ['booleans'],
+      status: ExerciseState.Locked,
     },
   ],
   [
@@ -56,6 +62,7 @@ export const data: ExerciseLayersData = [
       uuid: 'ad01ae6c-bdcf-4512-b55b-c552e2b2e41d',
       concepts: ['multiple-clause-functions', 'guards', 'default-arguments'],
       prerequisites: ['conditionals', 'string-literals'],
+      status: ExerciseState.Locked,
     },
   ],
   [
@@ -78,6 +85,7 @@ export const data: ExerciseLayersData = [
       uuid: '335df1cf-6aba-4ab6-b5c3-b4305ade09a4',
       concepts: ['strings'],
       prerequisites: ['lists', 'pattern-matching'],
+      status: ExerciseState.Locked,
     },
     {
       index: 10,
@@ -111,6 +119,7 @@ export const data: ExerciseLayersData = [
       uuid: 'b6ae6cb4-f908-436f-916c-fa847e4a82fb',
       concepts: ['nil', 'if-conditional'],
       prerequisites: ['strings', 'booleans'],
+      status: ExerciseState.Locked,
     },
     {
       index: 12,
@@ -118,6 +127,7 @@ export const data: ExerciseLayersData = [
       uuid: '45f3a08f-fe0f-4e3e-b325-47bad3c74ea4',
       concepts: ['charlists', 'case'],
       prerequisites: ['lists', 'recursion', 'pattern-matching', 'guards'],
+      status: ExerciseState.Locked,
     },
     {
       index: 22,
@@ -125,6 +135,7 @@ export const data: ExerciseLayersData = [
       uuid: 'bec0b00f-816e-443a-af94-14ab4125e505',
       concepts: ['processes', 'pids'],
       prerequisites: ['atoms', 'recursion', 'pattern-matching', 'tuples'],
+      status: ExerciseState.Locked,
     },
     {
       index: 25,
@@ -132,6 +143,7 @@ export const data: ExerciseLayersData = [
       uuid: '57198686-71c9-4f38-973a-a111435560e7',
       concepts: ['regular-expressions'],
       prerequisites: ['strings'],
+      status: ExerciseState.Locked,
     },
   ],
   [
@@ -141,6 +153,7 @@ export const data: ExerciseLayersData = [
       uuid: 'ba03df1d-20e4-4527-a8a7-afc97a834975',
       concepts: ['access-behaviour'],
       prerequisites: ['maps', 'strings', 'recursion', 'nil'],
+      status: ExerciseState.Locked,
     },
     {
       index: 15,
@@ -148,6 +161,7 @@ export const data: ExerciseLayersData = [
       uuid: '994c8760-8a58-4ef3-8a28-a0f9182f4d9f',
       concepts: ['bitstrings', 'tail-call-recursion'],
       prerequisites: ['charlists', 'recursion', 'pattern-matching'],
+      status: ExerciseState.Locked,
     },
     {
       index: 16,
@@ -210,6 +224,7 @@ export const data: ExerciseLayersData = [
       uuid: 'f660a2e1-bb50-4626-8d3f-6cd7c44fa8db',
       concepts: ['errors', 'try-rescue'],
       prerequisites: ['anonymous-functions', 'pattern-matching', 'structs'],
+      status: ExerciseState.Locked,
     },
     {
       index: 23,
@@ -217,6 +232,7 @@ export const data: ExerciseLayersData = [
       uuid: '3e2f8bc6-20b4-4e8f-a658-9c270342208e',
       concepts: ['streams', 'pipe-operator', 'ranges'],
       prerequisites: ['enum', 'tuples', 'if-conditional'],
+      status: ExerciseState.Locked,
     },
     {
       index: 24,
@@ -224,6 +240,7 @@ export const data: ExerciseLayersData = [
       uuid: '1d6be9ec-5b5c-4c81-9bfa-2e3fc78c978f',
       concepts: ['agent'],
       prerequisites: ['processes', 'maps', 'structs'],
+      status: ExerciseState.Locked,
     },
     {
       index: 26,
@@ -231,6 +248,7 @@ export const data: ExerciseLayersData = [
       uuid: 'f8208858-901e-4e5a-9f34-689bf8ad156c',
       concepts: ['list-comprehensions'],
       prerequisites: ['enum', 'lists', 'maps', 'keyword-lists', 'tuples'],
+      status: ExerciseState.Locked,
     },
   ],
   [
@@ -240,6 +258,7 @@ export const data: ExerciseLayersData = [
       uuid: '9f698b46-fe8d-4641-a928-bcebfd9678df',
       concepts: ['exceptions'],
       prerequisites: ['access-behaviour', 'errors'],
+      status: ExerciseState.Locked,
     },
     {
       index: 21,
@@ -247,6 +266,7 @@ export const data: ExerciseLayersData = [
       uuid: '68a8f721-5db4-4b31-95c8-fa6c38f64327',
       concepts: ['try-rescue-else-after'],
       prerequisites: ['io', 'try-rescue'],
+      status: ExerciseState.Locked,
     },
   ],
 ]
