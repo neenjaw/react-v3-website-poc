5a9db30e121b999d8c2076e2942b4b9ec732b547
Tim Austin <tim@neenjaw.com>
2020-10-02 15:42:06 -0600

    import cleaned up

--- src/graph-view/Exercise.tsx
+++ src/graph-view/Exercise.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { ExerciseCompleteIcon } from './ExerciseCompleteIcon'
 
 import { Exercise as IExercise, ExerciseState } from './exercise-types'
